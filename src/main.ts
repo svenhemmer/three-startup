dc27ec5eecf2f93eadd32be00250282c45b2c100
Sven Hemmer <sven.hemmer@web.de>
2024-05-03 08:03:45 +0200

    Add floor and controls

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import './style.css'
 import *  as THREE from 'three';
-import { WebGL } from 'three/examples/jsm/Addons.js';
+import { WebGL, OrbitControls } from 'three/examples/jsm/Addons.js';
 
-const createScene = () => {
+const createStage = () => {
 
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  let _camera: THREE.Camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  let _pause = false;
   
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
@@ -16,19 +18,48 @@ const createScene = () => {
   const cube = new THREE.Mesh(geometry, material );
   scene.add(cube);
   
-  camera.position.z = 5;
+  _camera.position.set(10, 15, -10);
+
+  new OrbitControls( _camera, document.body );
+
+  const generateSceneFloor = () => {
+    const geometry = new THREE.PlaneGeometry( 100, 100 );
+    const material = new THREE.MeshBasicMaterial( { side: THREE.DoubleSide, visible: false } );
+    const floor = new THREE.Mesh(geometry, material);
+    floor.rotateX(-Math.PI / 2 );
+    scene.add(floor);
+
+    const grid = new THREE.GridHelper(100, 100);
+    scene.add(grid);
+  }
+
+  generateSceneFloor();
   
   const animate = () => {
+    if (_pause) {
+      return;
+    }
     requestAnimationFrame( animate );
-    renderer.render( scene, camera );
+    renderer.render( scene, _camera );
     cube.rotation.x += .01;
     cube.rotation.y += .01;
   }
   animate();
+
+  const pause = () => _pause = true;
+  const resume = () => _pause = false;
+  const add = (object: THREE.Object3D<THREE.Object3DEventMap>[]) => scene.add(...object);
+  const remove = (object: THREE.Object3D<THREE.Object3DEventMap>) => scene.remove(object);
+
+  const changeCamera = (camera: THREE.Camera) => _camera = camera;
+
+  return {
+    pause, resume, add, remove, changeCamera
+  }
 }
 
 if ( WebGL.isWebGLAvailable() ) {
-  createScene();
+  createStage();
 } else {
   const warning = WebGL.getWebGLErrorMessage();
   document.getElementById('app')?.appendChild(warning);
